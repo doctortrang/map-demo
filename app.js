f91debe033ef4d665a0dbe878be1c0de25377743
Trang Trieu <79191593+doctortrang@users.noreply.github.com>
2023-05-07 21:45:07 +0700

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchBox = document.getElementById("searching")
+
+searchBox.addEventListener('click', function() {
+    searchBox.placeholder = '';
+    searchBox.style.backgroundImage = 'none';
+});
+
