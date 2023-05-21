84a9c44ef89c30882e76085c9d922c855139816c
Trang Trieu <79191593+doctortrang@users.noreply.github.com>
2023-05-21 15:09:19 +0700

    update

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ searchBox.addEventListener('click', function() {
     searchBox.style.backgroundImage = 'none';
 });
 
+const people = document.getElementById("people")
+const map = document.getElementById("map")
+const climate = document.getElementById("climate")
+
+const mapIcon = document.getElementById("map-icon")
+mapIcon.addEventListener("click", function() {
+    people.style.display = "none"
+    climate.style.display = "none"
+    map.style.display = "block";
+})
+
+const peopleIcon = document.getElementById("people-icon")
+peopleIcon.addEventListener("click", function() {
+    map.style.display = "none";
+    climate.style.display = "none"
+    people.style.display = "block"
+})
+
+const climateIcon = document.getElementById("air-icon")
+climateIcon.addEventListener("click", function() {
+    map.style.display = "none";
+    people.style.display = "none"
+    climate.style.display = "block"
+})
+
+
+
+
+
+
+
