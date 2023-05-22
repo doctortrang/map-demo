2a7005f4973e37488ac3af4163497c9a7985fa4e
Trang Trieu <79191593+doctortrang@users.noreply.github.com>
2023-05-22 22:26:43 +0700

    update

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ searchBox.addEventListener('click', function() {
 const people = document.getElementById("people")
 const map = document.getElementById("map")
 const climate = document.getElementById("climate")
+const wind = document.getElementById("wind")
 
 const mapIcon = document.getElementById("map-icon")
 mapIcon.addEventListener("click", function() {
     people.style.display = "none"
     climate.style.display = "none"
+    wind.style.display = "none"
     map.style.display = "block";
 })
 
@@ -20,17 +22,25 @@ const peopleIcon = document.getElementById("people-icon")
 peopleIcon.addEventListener("click", function() {
     map.style.display = "none";
     climate.style.display = "none"
+    wind.style.display = "none"
     people.style.display = "block"
 })
 
 const climateIcon = document.getElementById("air-icon")
 climateIcon.addEventListener("click", function() {
     map.style.display = "none";
-    people.style.display = "none"
-    climate.style.display = "block"
+    people.style.display = "none";
+    wind.style.display = "none";
+    climate.style.display = "block";
 })
 
-
+const windIcon = document.getElementById("wind-icon")
+windIcon.addEventListener("click", function() {
+    map.style.display = "none";
+    people.style.display = "none"
+    climate.style.display = "none"
+    wind.style.display = "block"
+})
 
 
 
